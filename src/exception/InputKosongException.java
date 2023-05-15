9bec86ee022951b8c7df5cadb89b811eea342bc6
Alfa <210711378@students.uajy.ac.id>
2023-05-15 17:30:56 +0700

    copy dari tgs 9

---
+++ src/exception/InputKosongException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception;
+// Nama : Vincentius Kenton
+// NPM : 210711307
+
+// Nama : Alfa Nada Yulaswara
+// NPM : 210711378
+
+public class InputKosongException extends Exception{
+    public String message(){
+        return "FIELD INPUT TIDAK BOLEH KOSONG";
+    }
+}
---
+++ src/exception/NoIdPustakaException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package exception;
+// Nama : Vincentius Kenton
+// NPM : 210711307
+
+// Nama : Alfa Nada Yulaswara
+// NPM : 210711378
+
+public class NoIdPustakaException extends Exception{
+    public String message(){
+        return "Id Pustaka harus diantara 5-8 digit";
+    }
+}
